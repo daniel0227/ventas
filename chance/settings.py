f4a79713a8feeb1fa91a5692c53af7fb332ca1da
Daniel <daniel_cardona305@hotmail.com>
2024-12-10 18:43:51 -0500

    Configuración de WhiteNoise y actualización de archivos estáticos

--- chance/settings.py
+++ chance/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Añádelo aquí
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -102,6 +103,11 @@ USE_TZ = True  # Habilitar manejo de zonas horarias
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Configuración del almacenamiento de archivos estáticos con WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Asegúrate de que Django reconozca los archivos estáticos de tu aplicación
 STATICFILES_DIRS = [
