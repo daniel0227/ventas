57750d9c76ecf32526996867250fce860c231704
Daniel <daniel_cardona305@hotmail.com>
2025-04-04 10:46:53 -0500

    add production domain

--- chance/settings.py
+++ chance/settings.py
@@ -75,27 +75,27 @@ WSGI_APPLICATION = 'chance.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'gqwmsJPkWFgDUaWftTzCEiqdUFCAvBXx',
-#         'HOST': 'autorack.proxy.rlwy.net',
-#         'PORT': '41491',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Motor de base de datos
-        'NAME': 'db_chance',                       # Nombre de la base de datos
-        'USER': 'postgres',                        # Usuario de la base de datos
-        'PASSWORD': 'Antonia2409.',                # Contraseña del usuario
-        'HOST': 'localhost',                       # Host de la base de datos (local o remoto)
-        'PORT': '5432',                            # Puerto estándar para PostgreSQL
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gqwmsJPkWFgDUaWftTzCEiqdUFCAvBXx',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '41491',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',  # Motor de base de datos
+#         'NAME': 'db_chance',                       # Nombre de la base de datos
+#         'USER': 'postgres',                        # Usuario de la base de datos
+#         'PASSWORD': 'Antonia2409.',                # Contraseña del usuario
+#         'HOST': 'localhost',                       # Host de la base de datos (local o remoto)
+#         'PORT': '5432',                            # Puerto estándar para PostgreSQL
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
